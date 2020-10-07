968405d3628ecde9933aca5c20513eaace993f04
Tacobell <franklinlantigua90@gmail.com>
2020-10-07 00:53:32 -0400

    -Added the ability to send things with intents

--- app/src/main/java/edu/temple/paletteapp/ColorViewAdapter.java
+++ app/src/main/java/edu/temple/paletteapp/ColorViewAdapter.java
@@ -2,25 +2,30 @@ package edu.temple.paletteapp;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 
-
 public class ColorViewAdapter extends BaseAdapter {
-
     Context context;
     ArrayList<String> items;
     ArrayList<String> colors;
 
+
     public ColorViewAdapter(Context context, ArrayList<String> items, ArrayList<String> colors) {
         this.context = context;
         this.items = items;
-        this.colors = colors;
+        this.colors= colors;
+
     }
 
     @Override
@@ -44,14 +49,22 @@ public class ColorViewAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView;
 
+        if(convertView == null) {
 
-        TextView textView = new TextView(context);
+            textView = new TextView(context);
+            textView.setLayoutParams(new GridView.LayoutParams(359,300));
+            textView.setGravity(Gravity.CENTER);
+            textView.setTypeface(null, Typeface.BOLD);
+
+        } else {
+            textView = (TextView) convertView;
+        }
         textView.setText(getItem(position).toString());
         textView.setBackgroundColor(Color.parseColor(getColor(position).toString()));
+
         return textView;
 
     }
-
-
 }
--- app/src/main/java/edu/temple/paletteapp/PaletteActivity.java
+++ app/src/main/java/edu/temple/paletteapp/PaletteActivity.java
@@ -1,14 +1,80 @@
 package edu.temple.paletteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class PaletteActivity extends AppCompatActivity {
+GridView gridView;
+
+    ArrayList<String> item = new ArrayList<>();
+    ArrayList<String> color = new ArrayList<>();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        item.add("Kyo Kusanagi");
+        item.add("Geese Howard");
+        item.add("Leona Heidern");
+        item.add("Kula Diamond");
+        item.add("Billy Kane");
+        item.add("Yuri Sakazaki");
+        item.add("Ash Crimson");
+        item.add("Robert Garcia");
+        item.add("Ryo Sakazaki");
+        item.add("Athena Asamiya");
+
+        color.add("#D45F00");
+        color.add("#D813EC");
+        color.add("#56A961");
+        color.add("#10E1EF");
+        color.add("#6969FF");
+        color.add("#FFF6EC");
+        color.add("#FF2418");
+        color.add("#8C7696");
+        color.add("#FFAE00");
+        color.add("#FF87A9");
+        gridView = findViewById(R.id.griddle);
+
+
+
+
+        final BaseAdapter adapter = new ColorViewAdapter(this, item, color);
+
+        gridView.setAdapter(adapter);
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(PaletteActivity.this, CanvasActivity.class);
+                intent.putExtra("color", color.get(position).toString());
+                intent.putExtra("text", item.get(position).toString());
+
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 }
